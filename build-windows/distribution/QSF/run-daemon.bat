7798cd9175184b882f36b09fd1f75c95c0baa4dd
qsfdev <qsfcoin@proton.me>
2025-10-14 10:35:42 -0700

    Remove build artifacts from repo

--- build-windows/distribution/QSF/run-daemon.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo Starting QSF Daemon...
-qsf.exe --rpc-bind-ip=127.0.0.1 --rpc-bind-port=18072
-pause
--- build-windows/distribution/QSF/run-gui-miner.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo Starting QSF GUI Miner...
-qsf-gui-miner.exe
-pause
